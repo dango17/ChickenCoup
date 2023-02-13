468b4ace09930dc4e35de75726f0f3585af4f76f
Double-Zeta <55756139+Double-Zeta@users.noreply.github.com>
2023-02-13 14:00:19 +0000

    Resolution functionality

---
+++ ChickenCoup/Assets/Scripts/UI/Scenemanager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scenemanager : MonoBehaviour
+{
+    public GameObject playBtn;
+    public GameObject optBtn;
+    public GameObject extBtn;
+    public GameObject backBtn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //default active menu layout on game start
+        playBtn.SetActive(true);
+        optBtn.SetActive(true);
+        extBtn.SetActive(true);
+        backBtn.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Options()
+    {
+        playBtn.SetActive(false);
+        optBtn.SetActive(false);
+        extBtn.SetActive(false);
+        backBtn.SetActive(true);
+    }
+
+    public void backButton()
+    {
+        playBtn.SetActive(true);
+        optBtn.SetActive(true);
+        extBtn.SetActive(true);
+        backBtn.SetActive(false);
+    }
+}
