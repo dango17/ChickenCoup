f936c8ba91a96ba43fa0fa8c8a5f7ad5a70f0c06
dango17 <danieloldham75@gmail.com>
2023-04-01 18:45:52 +0100

    Eating Foods System

--- Assets/Scripts/Player/PickupItem.cs
+++ Assets/Scripts/Player/PickupItem.cs
@@ -3,6 +3,7 @@
 // Created On: 19/03/2023
 
 using UnityEngine;
+using System.Collections; 
 
 namespace DO
 {
@@ -10,33 +11,54 @@ namespace DO
     //More user firendly than the last 'pickupable.cs' script
     public class PickupItem : MonoBehaviour
     {
+        [Header("Current GameObject")]
         [SerializeField] public GameObject currentObject;
+        [Header("PickupPoint Transform")]
         [SerializeField] public Transform pickupPoint;
+        [Header("Input-Handler")]
         [SerializeField] public InputHandler inputHandler;
-
-        public bool isHoldingItem = false; 
+        [Header("What Item Is This?")]
+        [SerializeField] public bool canMove;
+        [SerializeField] public bool isFood;
+        [SerializeField] public bool isSugary;
+        [SerializeField] public bool isAlcohol;
+        [Header("Eaten ParticleEffect")]
+        public GameObject EatEffectPrefab;
 
         #region Pickup Items
         //OTE && OTS Functions are identically to eachother 
         public void OnTriggerEnter(Collider other)
         {
-            if (other.tag == "Player" && inputHandler.isGrabbing == true)
+            if (other.tag == "Player")
             {
-                currentObject.GetComponent<Rigidbody>().isKinematic = true;
-                currentObject.GetComponent<BoxCollider>().enabled = false;
-                currentObject.transform.parent = pickupPoint.transform;
-                currentObject.transform.localPosition = Vector3.zero;
-                isHoldingItem = true;
-                
-                if(isHoldingItem)
+                //Player can pick up item as normal if canMove == true
+                if (inputHandler.isGrabbing == true && canMove == true)
                 {
-                    //Do nothing
+                    currentObject.GetComponent<Rigidbody>().isKinematic = true;
+                    currentObject.GetComponent<BoxCollider>().enabled = false;
+                    currentObject.transform.parent = pickupPoint.transform;
+                    currentObject.transform.localPosition = Vector3.zero;
+ 
                 }
+                //Player will temp hold food, held long enough == Eaten
+                if (inputHandler.isGrabbing == true && isFood == true)
+                {
+                    currentObject.GetComponent<Rigidbody>().isKinematic = true;
+                    currentObject.GetComponent<BoxCollider>().enabled = false;
+                    currentObject.transform.parent = pickupPoint.transform;
+                    currentObject.transform.localPosition = Vector3.zero;
+
+                    //Temp spawn more eggs here
+
+                    Invoke("DestoryCurrentObject", 0.75f); 
+                }
+                //Do Sugary Foods Here (speed boost player)
+
+                //Do Alcohol Foods Here (Add chromatic abbrasion)
             }
             else if (inputHandler.isGrabbing == false)
             {
                 DropItem();
-                isHoldingItem = false; 
             }
         }
 
@@ -48,7 +70,7 @@ namespace DO
                 currentObject.GetComponent<BoxCollider>().enabled = false;
                 currentObject.transform.parent = pickupPoint.transform;
                 currentObject.transform.localPosition = Vector3.zero;
-                isHoldingItem = true; 
+
             }
             else if (inputHandler.isGrabbing == false) 
             {
@@ -56,6 +78,22 @@ namespace DO
             }
         }
         #endregion
+
+        public void DestoryCurrentObject()
+        {
+            GameObject particleEffect = Instantiate(EatEffectPrefab, currentObject.transform.position, Quaternion.identity);
+            Destroy(currentObject);
+        }
+
+        private IEnumerator DestroyParticleEffect(GameObject EatEffectPrefab, ParticleSystem particleSystem)
+        {
+            while (particleSystem.IsAlive())
+            {
+                yield return null;
+            }
+            Destroy(EatEffectPrefab);
+        }
+
 
         public void DropItem()
         {
