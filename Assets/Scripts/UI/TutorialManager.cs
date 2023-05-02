867c514d9a68a9aed2592c6b6eb930c2cb9ac972
dango17 <danieloldham75@gmail.com>
2023-05-02 19:13:53 +0100

    Breakable Pot

--- Assets/Scripts/UI/TutorialManager.cs
+++ Assets/Scripts/UI/TutorialManager.cs
@@ -2,6 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Author: Daniel Oldham/s1903729
+// Collaborator: N/A
+// Created On: 30/04/2023
+
 public class TutorialManager : MonoBehaviour
 {
    
