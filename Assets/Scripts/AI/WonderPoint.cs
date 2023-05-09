9df207a46c41f304d52fb887fe4c3876afd15550
Liam Bansal <55756009+liambansal@users.noreply.github.com>
2023-05-09 15:15:25 +0100

    Wonder Points Move (WIP)
    Wonder points now move between the farmer and their destination along a fixed path.

--- Assets/Scripts/AI/WonderPoint.cs
+++ Assets/Scripts/AI/WonderPoint.cs
@@ -2,8 +2,6 @@
 // Collaborator: N/A
 // Created On: 9/5/2023 
 
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
@@ -17,9 +15,68 @@ public class WonderPoint : MonoBehaviour {
     }
 
     private bool isTouching = false;
+    [SerializeField, Tooltip("How quickly the object moves away from or towards " +
+        "the farmer.")]
+    private int moveSpeed = 3;
+    [SerializeField, Tooltip("The maximum range between the wonder point and " +
+        "farmer.")]
+    private int displacement = 3;
+    private float distanceFromFarmerToFurthestPoint = 0.0f;
+    private float distanceToFurthestPoint = 0.0f;
+    private Vector3 moveDirection = Vector3.zero;
+    private Vector3 furthestPosition = Vector3.zero;
+    private Vector3 verticalOffset = Vector3.zero;
     [SerializeField, Tooltip("Mark any layers that should trigger collisions " +
         "with this game-object.")]
-    private LayerMask collisionLayers = default;  
+    private LayerMask collisionLayers = default;
+    private Rigidbody rigidBody = null;
+    private BoxCollider boxCollider = null;
+    private Farmer farmer = null;
+
+    private void Awake() {
+        rigidBody = GetComponent<Rigidbody>();
+        boxCollider = GetComponent<BoxCollider>();
+        verticalOffset = Vector3.up * boxCollider.bounds.size.y;
+    }
+
+    private void Start() {
+        farmer = GameObject.FindGameObjectWithTag("Farmer").GetComponent<Farmer>();
+        moveDirection = new Vector3(transform.position.x - farmer.transform.position.x,
+            0,
+            transform.position.z - farmer.transform.position.z).normalized;
+        furthestPosition = farmer.transform.position + moveDirection * displacement + verticalOffset;
+        distanceFromFarmerToFurthestPoint = Mathf.Clamp(Vector3.Distance(farmer.transform.position + verticalOffset, furthestPosition),
+            0.0f,
+            displacement);
+        transform.position = farmer.transform.position + verticalOffset;
+        distanceToFurthestPoint = Mathf.Clamp(Vector3.Distance(transform.position, furthestPosition),
+            0.0f,
+            displacement);
+    }
+
+    private void Update() {
+        furthestPosition = farmer.transform.position + moveDirection * displacement + verticalOffset;
+        distanceFromFarmerToFurthestPoint = Mathf.Clamp(Vector3.Distance(farmer.transform.position + verticalOffset, furthestPosition),
+            0.0f,
+            displacement);
+        distanceToFurthestPoint = Mathf.Clamp(Vector3.Distance(transform.position, furthestPosition),
+            0.0f,
+            displacement);
+        float moveDistance = distanceToFurthestPoint / distanceFromFarmerToFurthestPoint;
+        moveDistance = Mathf.Clamp(moveDistance, 0.0f, 1.0f);
+
+        transform.position = new Vector3(Mathf.Lerp(farmer.transform.position.x,
+                furthestPosition.x,
+                moveDistance),
+            farmer.transform.position.y + boxCollider.bounds.size.y,
+            Mathf.Lerp(farmer.transform.position.z,
+                furthestPosition.z,
+                moveDistance));
+        
+        distanceToFurthestPoint = Mathf.Clamp(Vector3.Distance(transform.position, furthestPosition),
+            0.0f,
+            displacement);
+    }
 
     private void OnTriggerEnter(Collider other) {
         TriggeredCollision(other, true);
