fb4d9bf790d8694a27a1ff96688bb3ca310d7290
Liam Bansal <55756009+liambansal@users.noreply.github.com>
2023-05-11 14:11:49 +0100

    Update WonderPointManager.cs
    The wonder point manager now shows the direction towards the point in open space that's the furthest away from the farmer.

--- Assets/Scripts/AI/WonderPointManager.cs
+++ Assets/Scripts/AI/WonderPointManager.cs
@@ -16,11 +16,20 @@ public class WonderPointManager : MonoBehaviour {
         get;
         private set;
     }
+    /// <summary>
+    /// A direction towards the point, among the largest consecutive set of 
+    /// points in open space, that's the furthest away from the farmer.
+    /// Sometimes provides a better direction to move towards to reach open 
+    /// space than the set's average direction.
+    /// </summary>
+    public Vector3 DirectionToFurthestPointInOpenSpace {
+        get;
+        private set;
+    }
 
 	[SerializeField, Tooltip("Set the order of wonder points starting with " +
 		"North and going clockwise.")]
     private WonderPoint[] wonderPoints = new WonderPoint[8];
-
     private GameObject farmer = null;
 
     private void Awake() {
@@ -36,12 +45,16 @@ public class WonderPointManager : MonoBehaviour {
         AverageDirectionToOpenSpace = Vector3.zero;
         int consecutiveEmptyPoints = 0;
         int highestConsecutiveEmptyPoints = 0;
+        float furthestDistance = 0;
+        GameObject furthestPoint = null;
         Vector3 averageDirectionToOpenSpace = Vector3.zero;
 
         foreach (WonderPoint wonderPoint in wonderPoints) {
             if (wonderPoint.IsTouching) {
                 consecutiveEmptyPoints = 0;
+                furthestDistance = 0;
                 averageDirectionToOpenSpace = Vector3.zero;
+                furthestPoint = null;
                 continue;
             }
 
@@ -52,7 +65,12 @@ public class WonderPointManager : MonoBehaviour {
             // length than the previous set.
             if (consecutiveEmptyPoints > highestConsecutiveEmptyPoints) {
                 highestConsecutiveEmptyPoints = consecutiveEmptyPoints;
-                AverageDirectionToOpenSpace = averageDirectionToOpenSpace;
+                AverageDirectionToOpenSpace = averageDirectionToOpenSpace.normalized;
+                GetFurthestPoint(wonderPoint.gameObject);
+
+                if (furthestPoint) {
+                    DirectionToFurthestPointInOpenSpace = (furthestPoint.transform.position - transform.position).normalized;
+                }
             } else if (consecutiveEmptyPoints == highestConsecutiveEmptyPoints) {
                 float dotProductOfNewDirection = Vector3.Dot(farmer.transform.forward, averageDirectionToOpenSpace);
                 float dotProductOfCurrentDirection = Vector3.Dot(farmer.transform.forward, AverageDirectionToOpenSpace);
@@ -60,12 +78,27 @@ public class WonderPointManager : MonoBehaviour {
                 // Finds which set of consecuutive wonder points are closest
                 // in direction to the farmer's forward direction.
                 if (dotProductOfCurrentDirection < dotProductOfNewDirection) {
-                    AverageDirectionToOpenSpace = averageDirectionToOpenSpace;
-				}
+                    AverageDirectionToOpenSpace = averageDirectionToOpenSpace.normalized;
+                    GetFurthestPoint(wonderPoint.gameObject);
+
+                    if (furthestPoint) {
+                        DirectionToFurthestPointInOpenSpace = (furthestPoint.transform.position - transform.position).normalized;
+                    }
+                }
 			}
         }
 
         Debug.DrawLine(transform.position, transform.position + AverageDirectionToOpenSpace * 2);
+        Debug.DrawLine(transform.position, transform.position + DirectionToFurthestPointInOpenSpace * 2);
+
+        void GetFurthestPoint(GameObject point) {
+            float distanceToPoint = Vector3.Distance(transform.position, point.transform.position);
+
+            if (distanceToPoint > furthestDistance) {
+                furthestDistance = distanceToPoint;
+                furthestPoint = point;
+            }
+        }
     }
 
     /// <summary>
