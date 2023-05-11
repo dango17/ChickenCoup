21d6ae84e55969343fedf08a85b30a3ff9ada206
Liam Bansal <55756009+liambansal@users.noreply.github.com>
2023-05-11 13:20:37 +0100

    Better Wonder Point Manager
    The wonder point manager now shows more accurately exactly where the most open space around the farmer is.

--- Assets/Scripts/AI/WonderPointManager.cs
+++ Assets/Scripts/AI/WonderPointManager.cs
@@ -9,27 +9,61 @@ using UnityEngine;
 /// </summary>
 public class WonderPointManager : MonoBehaviour {
     /// <summary>
-    /// The sum of directions to all wonder points that are not triggering a 
-    /// collision.
+    /// The average direction towards the largest set of consecutive wonder 
+    /// points that aren't triggering a collisions.
     /// </summary>
     public Vector3 AverageDirectionToOpenSpace {
         get;
         private set;
     }
 
-    private WonderPoint[] wonderPoints = new WonderPoint[8];
+	[SerializeField, Tooltip("Set the order of wonder points starting with " +
+		"North and going clockwise.")]
+    private WonderPoint[] wonderPoints = new WonderPoint[16];
+
+    private GameObject farmer = null;
 
     private void Awake() {
         wonderPoints = GetComponentsInChildren<WonderPoint>();
     }
 
-    private void FixedUpdate() {
+	private void Start() {
+        farmer = GameObject.FindGameObjectWithTag("Farmer");
+    }
+
+	private void FixedUpdate() {
         AverageDirectionToOpenSpace = Vector3.zero;
+        int consecutiveEmptyPoints = 0;
+        int highestConsecutiveEmptyPoints = 0;
+        Vector3 averageDirectionToOpenSpace = Vector3.zero;
 
         foreach (WonderPoint wonderPoint in wonderPoints) {
-            if (!wonderPoint.IsTouching) {
-                AverageDirectionToOpenSpace += (wonderPoint.transform.position - transform.position).normalized;
+            if (wonderPoint.IsTouching) {
+                consecutiveEmptyPoints = 0;
+                averageDirectionToOpenSpace = Vector3.zero;
+                continue;
             }
+
+            ++consecutiveEmptyPoints;
+            averageDirectionToOpenSpace += (wonderPoint.transform.position - transform.position).normalized;
+
+            // Checks if the new set of consecutive points are greater in
+            // length than the previous set.
+            if (consecutiveEmptyPoints > highestConsecutiveEmptyPoints) {
+                highestConsecutiveEmptyPoints = consecutiveEmptyPoints;
+                AverageDirectionToOpenSpace = averageDirectionToOpenSpace;
+            } else if (consecutiveEmptyPoints == highestConsecutiveEmptyPoints) {
+                float dotProductOfNewDirection = Vector3.Dot(farmer.transform.forward, averageDirectionToOpenSpace);
+                float dotProductOfCurrentDirection = Vector3.Dot(farmer.transform.forward, AverageDirectionToOpenSpace);
+
+                // Finds which set of consecuutive wonder points are closest
+                // in direction to the farmer's forward direction.
+                if (dotProductOfCurrentDirection < dotProductOfNewDirection) {
+                    AverageDirectionToOpenSpace = averageDirectionToOpenSpace;
+				}
+			}
         }
+
+        Debug.DrawLine(transform.position, transform.position + AverageDirectionToOpenSpace * 2, Color.red, 2.0f);
     }
 }
