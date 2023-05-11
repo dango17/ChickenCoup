41d06a27e3416a9e6f546149eae6c74e8ff50ee3
Double-Zeta <55756139+Double-Zeta@users.noreply.github.com>
2023-05-11 14:11:23 +0100

    farmer fov visualiser

--- Assets/Scripts/AI/FarmerFoVvisual.cs
+++ Assets/Scripts/AI/FarmerFoVvisual.cs
@@ -9,13 +9,71 @@ public class FarmerFoVvisual : MonoBehaviour
 
     public float viewAngle;
 
+    public LayerMask targetMask;
+    public LayerMask obstacleMask;
+
+    [HideInInspector]
+    public List<Transform> visibleTargets = new List<Transform>();
+
+    public float meshResolution;
+
+    private void Start()
+    {
+        StartCoroutine("FindTargets", 0.2f);
+    }
+    IEnumerator FindTargets(float delay)
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(delay);
+            FindVisibleTargets();
+        }
+    }
+
+    private void Update()
+    {
+        DrawFieldOfView();
+    }
+    void FindVisibleTargets()
+    {
+        visibleTargets.Clear();
+        Collider[] targetsInViewRadius = Physics.OverlapSphere(transform.position, viewRadius, targetMask);
+
+        for(int i = 0; i < targetsInViewRadius.Length; i++)
+        {
+            Transform target = targetsInViewRadius[i].transform;
+            Vector3 dirToTarget = (target.position - transform.position).normalized;
+            if(Vector3.Angle (transform.forward,dirToTarget) < viewAngle/2)
+            {
+                float distToTarget = Vector3.Distance(transform.position, target.position);
+                if(!Physics.Raycast(transform.position, dirToTarget, distToTarget,obstacleMask))
+                {
+                    visibleTargets.Add(target);
+                }
+            }
+        }
+    }
+
+    void DrawFieldOfView()
+    {
+        int stepCount = Mathf.RoundToInt(viewAngle * meshResolution);
+        float stepAngleSize = viewAngle / stepCount;
+
+        for(int i = 0; i <= stepCount; i++)
+        {
+            float angle = transform.eulerAngles.y - viewAngle / 2 + stepAngleSize * i;
+            Debug.DrawLine(transform.position, transform.position + DirFromAngle(angle, true) * viewRadius, Color.red);
+        }
+    }
+
     public Vector3 DirFromAngle(float angleInDegrees, bool angleisGlobal)
     {
         if (!angleisGlobal)
         {
-
+            angleInDegrees += transform.eulerAngles.y;
         }
         return new Vector3(Mathf.Sin(angleInDegrees = Mathf.Deg2Rad),0,Mathf.Cos(angleInDegrees = Mathf.Deg2Rad));
+        
     }
 
    
--- Assets/Scripts/Editor/FarmerFoVvisualEditor.cs
+++ Assets/Scripts/Editor/FarmerFoVvisualEditor.cs
@@ -10,6 +10,24 @@ public class FarmerFoVvisualEditor : Editor
     {
         FarmerFoVvisual fov = (FarmerFoVvisual)target;
         Handles.color = Color.white;
+        //Vector3 viewAngleA = fov.DirFromAngle(-fov.viewAngle / 2, false);
+        //Vector3 viewAngleB = fov.DirFromAngle(fov.viewAngle / 2, false);
+        //Handles.DrawWireArc(fov.transform.position, Vector3.up, viewAngleA, fov.viewAngle, fov.viewRadius);
+        //Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngleA * fov.viewRadius);
+        //Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngleB * fov.viewRadius);
+
+        
         Handles.DrawWireArc(fov.transform.position, Vector3.up, Vector3.forward, 360, fov.viewRadius);
+        Vector3 viewAngleA = fov.DirFromAngle(-fov.viewAngle / 2, false);
+        Vector3 viewAngleB = fov.DirFromAngle(fov.viewAngle / 2, false);
+
+        Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngleA * fov.viewRadius);
+        Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngleB * fov.viewRadius);
+        
+        foreach (Transform visibleTarget in fov.visibleTargets)
+        {
+            Handles.color = Color.red;
+            Handles.DrawLine(fov.transform.position, visibleTarget.position);
+        }
     }
 }
